8fd7d13546099681423039af7d7a08276b726ed9
Phillip Rath <65727386+PhillipRt@users.noreply.github.com>
2023-07-20 14:27:11 +0000

    fix config flow

--- custom_components/whoop/config_flow.py
+++ custom_components/whoop/config_flow.py
@@ -6,42 +6,51 @@ from homeassistant.helpers import config_entry_oauth2_flow
 
 from .const import DOMAIN
 
-class OAuth2FlowHandler(
-    config_entry_oauth2_flow.AbstractOAuth2FlowHandler, domain=DOMAIN
-):
+class OAuth2FlowHandler(config_entry_oauth2_flow.AbstractOAuth2FlowHandler, domain=DOMAIN):
+    """Provide a config flow for OAuth2."""
+
     DOMAIN = DOMAIN
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
     @property
-    def logger(self):
+    def logger(self) -> logging.Logger:
         """Return logger."""
         return logging.getLogger(__name__)
 
     async def async_step_user(self, user_input=None):
         """Handle a flow start."""
-        if self.hass.config_entries.async_entries(DOMAIN):
-            return self.async_abort(reason="single_instance_allowed")
+        errors = {}
+        if user_input is not None:
+            return self.async_create_entry(
+                title=user_input["client_id"], 
+                data={
+                    "client_id": user_input["client_id"],
+                    "client_secret": user_input["client_secret"]
+                }
+            )
 
-        return await self.async_step_discovery(user_input)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required("client_id"): str,
+                    vol.Required("client_secret"): str,
+                }
+            ),
+            errors=errors,
+        )
 
-    async def async_step_discovery(self, user_input=None):
-        """Handle redirection to Whoop website for OAuth2 flow."""
-        return await self.async_step_auth()
+    async def async_step_creation(self, user_input=None):
+        """Handle a flow start."""
+        if user_input is not None:
+            return self.async_external_step_done(user_input)
 
-    async def async_step_auth(self, user_input=None):
-        """Handle redirection back from Whoop website after user's approval."""
-        return await self.async_step_code(user_input)
+        return await super().async_step_creation(user_input)
 
-    async def async_step_code(self, user_input=None):
-        """Handle the reception of the authorization code from Whoop."""
-        if user_input is None:
-            return self.async_external_step_done(next_step_id="creation")
+    async def async_oauth_create_entry(self, data: dict) -> dict:
+        """Create an entry for the flow.
 
-        return await self.async_step_creation(user_input)
-
-    async def async_step_creation(self, user_input=None, error=None):
-        """Handle callback from Whoop API."""
-        if user_input is None:
-            return self.async_external_step_done(next_step_id="creation")
-
-        return await self.async_oauth_create_entry(user_input)
+        Ok to override if you want to provide extra info for the creation of
+        the entry.
+        """
+        return self.async_create_entry(title=self.flow_impl.name, data=data)
